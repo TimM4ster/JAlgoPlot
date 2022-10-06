48f87cbc9deb8a4f0fb19dcdb30dfe59be31a409
TimM4ster <timmichael.krieg@gmail.com>
2022-10-06 22:47:04 +0200

    More javadoc for SortingState.java and added Pair class

--- src/main/java/algorithms/sorting/SortingState.java
+++ src/main/java/algorithms/sorting/SortingState.java
@@ -1,6 +1,7 @@
 package algorithms.sorting;
 
 import algorithms.State;
+import datastructure.Pair;
 
 /**
  * An object of this class extends the State class such that it represents a state of a sorting algorithm. In addition
@@ -25,17 +26,34 @@ public class SortingState extends State {
     private final int element;
 
     /**
+     * The elements that are either compared or swapped in this state, depending on the action that takes place.
+     */
+    private final Pair<Integer, Integer> elements;
+
+    /**
      * The action that takes place in this state. See the SortingAction enumeration for more details.
      */
     private final SortingAction action;
 
-    public SortingState(
-            int id, double time, int iteration, int[] array, int index, int element, SortingAction action
-    ) {
+    /**
+     * Constructor initializing a state of a sorting algorithm.
+     *
+     * @param id    The id (typically the number) of the state.
+     * @param time  The time elapsed from start of algorithm until this state.
+     * @param iteration The iteration of the algorithm in this state.
+     * @param array The current state of the array that is sorted by the algorithm.
+     * @param index The index of the element inside the array that is currently highlighted by the algorithm.
+     * @param element   The element inside the array that is currently highlighted by the algorithm.
+     * @param elements  The elements that are either compared or swapped in this state, depending on the action that
+     *                  takes place.
+     * @param action    The action that takes place in this state. See the SortingAction enumeration for more details.
+     */
+    public SortingState(int id, double time, int iteration, int[] array, int index, int element, Pair<Integer, Integer> elements, SortingAction action) {
         super(id, time, iteration);
         this.array = array;
         this.index = index;
         this.element = element;
+        this.elements = elements;
         this.action = action;
     }
 
@@ -64,6 +82,15 @@ public class SortingState extends State {
      */
     public int getElement() {
         return element;
+    }
+
+    /**
+     * Returns the elements that are either compared or swapped in this state, depending on the action that takes place.
+     *
+     * @return  The pair of elements.
+     */
+    public Pair<Integer, Integer> getElements() {
+        return elements;
     }
 
     /**
---
+++ src/main/java/datastructure/Pair.java
@@ -0,0 +1,45 @@
+package datastructure;
+
+/**
+ * An object of this class represents a pair of elements with different types.
+ *
+ * @param <F>   The type of the first element in the pair.
+ * @param <S>   The type of the second element in the pair.
+ */
+public class Pair<F, S> {
+
+    /**
+     * The first element of this pair.
+     */
+    public F first;
+
+    /**
+     * The second element of this pair.
+     */
+    public S second;
+
+    /**
+     * Constructor initializing each element in the pair. Use the {@code of} method to create an instance of this
+     * object.
+     *
+     * @param first The first element of this pair.
+     * @param second    The second element of this pair.
+     */
+    protected Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Method that constructs a pair object containing the given elements.
+     *
+     * @param first The first element of the pair.
+     * @param second    The second element of the pair.
+     * @return  The pair-object containing the two elements.
+     * @param <F>   The type of the first element.
+     * @param <S>   The type of the second element.
+     */
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+}
