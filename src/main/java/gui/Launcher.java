b98707bd834a69e0d5095c2831b4de20f5a7f1f3
TimM4ster <timmichael.krieg@gmail.com>
2022-10-10 19:54:43 +0200

    Finished first version of launcher

--- src/main/java/gui/Launcher.java
+++ src/main/java/gui/Launcher.java
@@ -1,7 +1,14 @@
 package gui;
 
+import gui.controllers.LauncherControls;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Launcher extends Application {
 
@@ -10,7 +17,14 @@ public class Launcher extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/scenes/Launcher.fxml")));
 
+        Scene scene = new Scene(root, LauncherControls.WIDTH, LauncherControls.HEIGHT);
+
+        stage.setTitle("Launcher");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
--- src/main/java/gui/controllers/LauncherControls.java
+++ src/main/java/gui/controllers/LauncherControls.java
@@ -1,7 +1,30 @@
 package gui.controllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
 /**
  * This class represents the controller of the launcher that launches the application.
  */
 public class LauncherControls {
+
+    /**
+     * The height of the launcher.
+     */
+    public static final double WIDTH = 500;
+
+    /**
+     * The width of the launcher.
+     */
+    public static final double HEIGHT = 450;
+
+    /**
+     * Exits the application.
+     *
+     * @param e The action event evoking the exit.
+     */
+    public void exitApplication(ActionEvent e) {
+        Platform.exit();
+    }
+
 }
